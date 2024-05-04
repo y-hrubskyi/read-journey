c57020fa7d5e2bb6297e85a08cf1160ce20342e5
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-04 10:29:37 +0300

    feat: create reusable BaseContainer component

---
+++ src/components/common/Layout/Layout.styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const BaseContainer = styled.div`
+  margin: 0 auto;
+  padding: ${(p) => p.theme.spacing(5)};
+
+  @media screen and (min-width: 375px) {
+    width: 375px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+    padding: ${(p) => p.theme.spacing(8)};
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 1440px;
+  }
+`;
