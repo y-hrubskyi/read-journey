c398e360112879e24d39df14c7360734f40ccd5a
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-05 19:56:55 +0300

    refactor: change import sprite > new URL for working dev & build (vite5)

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -1,12 +1,12 @@
-import sprite from "@/assets/icons/sprite.svg";
-
 import * as SC from "./Logo.styled";
 
 export const Logo = () => {
   return (
     <SC.Logo to="/">
       <SC.LogoIcon>
-        <use href={sprite + "#icon-logo"}></use>
+        <use
+          href={new URL("@/assets/icons/sprite.svg#icon-logo", import.meta.url)}
+        ></use>
       </SC.LogoIcon>
       <SC.LogoText>READ JOURNEY</SC.LogoText>
     </SC.Logo>
