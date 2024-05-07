2afafe156349698a610b9dc099fb3736092dd819
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-07 14:38:52 +0300

    setup: prepare to creating reusable PasswordField component
    - add eye-icons to sprite
    - upd Icons config

--- src/config/icons.js
+++ src/config/icons.js
@@ -2,4 +2,6 @@ export const Icons = {
   logo: new URL("@/assets/icons/sprite.svg#icon-logo", import.meta.url),
   correct: new URL("@/assets/icons/sprite.svg#icon-correct", import.meta.url),
   error: new URL("@/assets/icons/sprite.svg#icon-error", import.meta.url),
+  eyeOn: new URL("@/assets/icons/sprite.svg#icon-eye-on", import.meta.url),
+  eyeOff: new URL("@/assets/icons/sprite.svg#icon-eye-off", import.meta.url),
 };
