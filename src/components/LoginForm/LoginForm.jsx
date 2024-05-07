8800d7bd3fe27d7bf754a07e841a450bd7f3a2e7
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-07 15:06:36 +0300

    feat: create LoginForm component

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { loginSchema } from "@/config/validation/loginSchema";
+
+import { FormField } from "@/components/common/FormField/FormField";
+import { PasswordField } from "@/components/common/PasswordField/PasswordField";
+import {
+  EmailInput,
+  PasswordInput,
+  AuthPageLink,
+} from "@/components/common/AuthForm/AuthForm.styled";
+import * as SC from "./LoginForm.styled";
+
+export const LoginForm = () => {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, dirtyFields },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(loginSchema),
+  });
+
+  const togglePasswordShown = () => {
+    setPasswordShown((prevState) => !prevState);
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const isCorrectEmail = dirtyFields.email && !errors.email;
+  const hasErrorEmail = errors.email;
+  const isCorrectPassword = dirtyFields.password && !errors.password;
+  const hasErrorPassword = errors.password;
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <SC.FieldsWrapper>
+        <FormField
+          id="email"
+          label="Mail:"
+          isCorrect={isCorrectEmail}
+          correctMessage="Email is correct"
+          hasError={hasErrorEmail}
+          errorMessage={errors.email?.message}
+        >
+          <EmailInput
+            id="email"
+            {...register("email")}
+            type="email"
+            data-is-correct={isCorrectEmail}
+            data-has-error={hasErrorEmail}
+          />
+        </FormField>
+        <PasswordField
+          id="password"
+          label="Password:"
+          passwordShown={passwordShown}
+          onTogglePasswordShown={togglePasswordShown}
+          isCorrect={isCorrectPassword}
+          correctMessage="Password is secure"
+          hasError={hasErrorPassword}
+          errorMessage={errors.password?.message}
+        >
+          <PasswordInput
+            id="password"
+            {...register("password")}
+            type={passwordShown ? "text" : "password"}
+            data-is-correct={isCorrectPassword}
+            data-has-error={hasErrorPassword}
+          />
+        </PasswordField>
+      </SC.FieldsWrapper>
+
+      <SC.LoginSubmitBtn type="submit">Log In</SC.LoginSubmitBtn>
+      <AuthPageLink to="/register">Don&apos;t have an account?</AuthPageLink>
+    </form>
+  );
+};
