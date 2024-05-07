cbf0216d53c813729181e07244aef2dbd57ae65d
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-07 14:36:01 +0300

    feat: add validation styles and messages to FormField

--- src/components/common/FormField/FormField.jsx
+++ src/components/common/FormField/FormField.jsx
@@ -1,10 +1,36 @@
+import { Icons } from "@/config/icons";
+
 import * as SC from "./FormField.styled";
 
-export const FormField = ({ id, label, children }) => {
+export const FormField = ({
+  id,
+  label,
+  isCorrect,
+  correctMessage,
+  hasError,
+  errorMessage,
+  children,
+}) => {
   return (
     <SC.FieldWrapper>
       <SC.Label htmlFor={id}>{label}</SC.Label>
       {children}
+      {isCorrect && (
+        <>
+          <SC.CorrectIcon>
+            <use href={Icons.correct}></use>
+          </SC.CorrectIcon>
+          <SC.CorrectMessage>{correctMessage}</SC.CorrectMessage>
+        </>
+      )}
+      {hasError && (
+        <>
+          <SC.ErrorIcon>
+            <use href={Icons.error}></use>
+          </SC.ErrorIcon>
+          <SC.ErrorMessage>{errorMessage}</SC.ErrorMessage>
+        </>
+      )}
     </SC.FieldWrapper>
   );
 };
--- src/components/common/FormField/FormField.styled.js
+++ src/components/common/FormField/FormField.styled.js
@@ -35,7 +35,9 @@ export const Label = styled.label`
 
 export const BaseInput = styled.input`
   width: 100%;
-  padding: ${(p) => p.theme.spacing(3.25)};
+  padding-top: ${(p) => p.theme.spacing(3.25)};
+  padding-right: ${(p) => p.theme.spacing(10.75)};
+  padding-bottom: ${(p) => p.theme.spacing(3.25)};
 
   font-weight: 500;
   font-size: 12px;
@@ -44,9 +46,20 @@ export const BaseInput = styled.input`
   color: ${(p) => p.theme.colors.primaryText};
 
   background-color: ${(p) => p.theme.colors.secondaryBg};
-  border: 1px solid ${(p) => p.theme.colors.secondaryBg};
-  border-radius: ${(p) => p.theme.radii.field};
   outline: transparent;
+  border-radius: ${(p) => p.theme.radii.field};
+  border: 1px solid
+    ${(p) => {
+      if (p["data-is-correct"]) {
+        return p.theme.colors.green;
+      }
+
+      if (p["data-has-error"]) {
+        return p.theme.colors.red;
+      }
+
+      return "transparent";
+    }};
 
   transition: ${(p) => p.theme.transition("border-color")};
 
@@ -56,9 +69,61 @@ export const BaseInput = styled.input`
   }
 
   @media screen and (min-width: 768px) {
-    padding: ${(p) => p.theme.spacing(3.75)};
+    padding-top: ${(p) => p.theme.spacing(3.75)};
+    padding-right: ${(p) => p.theme.spacing(11.75)};
+    padding-bottom: ${(p) => p.theme.spacing(3.75)};
 
     font-size: 14px;
     line-height: 1.28571;
   }
+`;
+
+export const BaseValidInputIcon = styled.svg`
+  position: absolute;
+  top: 13px;
+  right: 16px;
+
+  width: 18px;
+  height: 18px;
+
+  @media screen and (min-width: 768px) {
+    top: 15px;
+    right: 18px;
+
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+export const CorrectIcon = styled(BaseValidInputIcon)`
+  fill: ${(p) => p.theme.colors.green};
+`;
+
+export const ErrorIcon = styled(BaseValidInputIcon)`
+  fill: ${(p) => p.theme.colors.red};
+`;
+
+export const BaseValidationMessage = styled.p`
+  margin-top: ${(p) => p.theme.spacing(1)};
+  margin-left: ${(p) => p.theme.spacing(3.5)};
+
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 1.2;
+  letter-spacing: -0.02em;
+
+  @media screen and (min-width: 768px) {
+    margin-top: ${(p) => p.theme.spacing(2)};
+
+    font-size: 12px;
+    line-height: 1.16667;
+  }
+`;
+
+export const CorrectMessage = styled(BaseValidationMessage)`
+  color: ${(p) => p.theme.colors.green};
+`;
+
+export const ErrorMessage = styled(BaseValidationMessage)`
+  color: ${(p) => p.theme.colors.red};
 `;
