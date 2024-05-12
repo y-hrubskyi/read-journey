c426b1595706385798387fddab3e44b3368d0ed4
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-12 22:40:12 +0300

    feat: create reusable Dashboard components
     - InfoWrapper
     - InfoTitle
     - AnotherPageLinksWrapper
     - AnotherPageLink
     - AnotherPageLinkBtn
     - ArrowIcon

--- src/components/common/Dashboard/Dashboard.styled.js
+++ src/components/common/Dashboard/Dashboard.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const BaseDashboard = styled.div`
   padding: ${(p) => p.theme.spacing(5)};
@@ -89,4 +90,61 @@ export const SubmitBtn = styled.button`
     font-size: 16px;
     line-height: 1.125;
   }
+`;
+
+export const InfoWrapper = styled.div`
+  padding: ${(p) => p.theme.spacing(5)};
+
+  background-color: ${(p) => p.theme.colors.secondaryBg};
+  border-radius: 12px;
+`;
+
+export const InfoTitle = styled.p`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 1;
+  letter-spacing: -0.02em;
+  color: ${(p) => p.theme.colors.primaryText};
+
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+`;
+
+export const AnotherPageLinksWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const AnotherPageLink = styled(Link)`
+  align-self: end;
+
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1.28571;
+  letter-spacing: -0.02em;
+  text-decoration: underline;
+  text-decoration-skip-ink: none;
+  color: ${(p) => p.theme.colors.secondaryText};
+
+  transition: ${(p) => p.theme.transition("color")};
+
+  &:hover,
+  &:focus {
+    color: ${(p) => p.theme.colors.primaryText};
+  }
+`;
+
+export const AnotherPageLinkBtn = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+
+  color: ${(p) => p.theme.colors.primaryText};
+`;
+
+export const ArrowIcon = styled.svg`
+  stroke: currentColor;
 `;
