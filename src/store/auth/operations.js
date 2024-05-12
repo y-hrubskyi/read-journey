5a998e2d9d7992e579515ac7b785befdbe3a7427
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-12 15:18:34 +0300

    feat: add notifications for registration
     - upd signUp operation: if error return message from server response
     - add toast.promise notification for signUp operation
     - disable register submit btn if isLoading

--- src/store/auth/operations.js
+++ src/store/auth/operations.js
@@ -10,7 +10,7 @@ export const signUp = createAsyncThunk(
       setAuthHeader(data.token);
       return data;
     } catch (error) {
-      return rejectWithValue(error.message);
+      return rejectWithValue(error.response.data.message);
     }
   }
 );
