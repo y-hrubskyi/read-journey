7ef8e9dc5f624a3bf3c69c525e21b333d6a9df56
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-13 21:18:28 +0300

    feat: add simple fetching recommended books
     - without pagination and filters

--- src/pages/RecommendedPage/RecommendedPage.jsx
+++ src/pages/RecommendedPage/RecommendedPage.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+
+import API from "@/services/axios";
+
 import { Filters } from "@/components/Filters/Filters";
 import { Guide } from "@/components/Guide/Guide";
 import { Quote } from "@/components/Quote/Quote";
@@ -5,10 +9,24 @@ import {
   PageContent,
   PageTitle,
 } from "@/components/common/PageContent/PageContent.styled";
+import { BookList } from "@/components/common/BookList/BookList";
 
 import * as SC from "./RecommendedPage.styled";
 
 const RecommendedPage = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await API.get("/books/recommend");
+        setBooks(data.results);
+      } catch (error) {
+        //
+      }
+    })();
+  }, []);
+
   const filterSubmit = (data) => {
     console.log(data);
   };
@@ -22,6 +40,7 @@ const RecommendedPage = () => {
       </SC.Dashboard>
       <PageContent>
         <PageTitle>Recommended</PageTitle>
+        {books.length > 0 && <BookList books={books} />}
       </PageContent>
     </>
   );
