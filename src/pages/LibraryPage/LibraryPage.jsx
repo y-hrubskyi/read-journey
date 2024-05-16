89e4bf557d093581babb26f140771b887b8ee653
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-16 07:26:50 +0300

    feat: add Dashboard & PageContent components for LibraryPage

--- src/pages/LibraryPage/LibraryPage.jsx
+++ src/pages/LibraryPage/LibraryPage.jsx
@@ -1,5 +1,24 @@
+import {
+  BookListPageContent,
+  ContentWrapper,
+  HeaderWrapper,
+  PageTitle,
+} from "@/components/common/PageContent/PageContent.styled";
+
+import * as SC from "./LibraryPage.styled";
+
 const LibraryPage = () => {
-  return <div>LibraryPage</div>;
+  return (
+    <>
+      <SC.Dashboard></SC.Dashboard>
+      <BookListPageContent>
+        <HeaderWrapper>
+          <PageTitle>My library</PageTitle>
+        </HeaderWrapper>
+        <ContentWrapper></ContentWrapper>
+      </BookListPageContent>
+    </>
+  );
 };
 
 export default LibraryPage;
