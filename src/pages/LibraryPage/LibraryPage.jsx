0d0bae751aeeb3b50bd1bff933c48fa13d8c0242
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-16 10:34:37 +0300

    @y-hrubskyi feat: add simple fetching own books

--- src/pages/LibraryPage/LibraryPage.jsx
+++ src/pages/LibraryPage/LibraryPage.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+
+import API from "@/services/axios";
+
 import { AddBook } from "@/components/AddBook/AddBook";
 import { RecommendedBooks } from "@/components/RecommendedBooks/RecommendedBooks";
 import {
@@ -6,10 +10,40 @@ import {
   HeaderWrapper,
   PageTitle,
 } from "@/components/common/PageContent/PageContent.styled";
+import { BookList } from "@/components/common/BookList/BookList";
+import { Loader } from "@/components/common/Loader/Loader";
+import { Placeholder } from "@/components/common/Placeholder/Placeholder";
+import { DarkenedText } from "@/components/common/Placeholder/Placeholder.styled";
 
 import * as SC from "./LibraryPage.styled";
 
 const LibraryPage = () => {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const { data } = await API.get(`/books/own`);
+        setBooks(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  const emptyData = books.length === 0;
+  const loading = !error && isLoading;
+  const hasError = !isLoading && error;
+  const content = !isLoading && !error && !emptyData;
+  const noData = !isLoading && !error && emptyData;
+
   return (
     <>
       <SC.Dashboard>
@@ -20,7 +54,18 @@ const LibraryPage = () => {
         <HeaderWrapper>
           <PageTitle>My library</PageTitle>
         </HeaderWrapper>
-        <ContentWrapper></ContentWrapper>
+        <ContentWrapper>
+          {content && <BookList books={books} />}
+          {loading && <Loader />}
+          {hasError && <Placeholder>Oops... {error}</Placeholder>}
+          {noData && (
+            <Placeholder>
+              To start training, add
+              <DarkenedText>some of your books</DarkenedText>
+              or from the recommended ones
+            </Placeholder>
+          )}
+        </ContentWrapper>
       </BookListPageContent>
     </>
   );
