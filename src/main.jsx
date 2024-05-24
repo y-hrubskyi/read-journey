10c4e677a3eda8947269dc354703e56c9c7ac7f9
y-hrubskyi <y.hrubskyi@gmail.com>
2024-05-24 10:03:56 +0300

    setup: upd routing for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import { App } from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/read-journey">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ThemeProvider theme={theme}>
